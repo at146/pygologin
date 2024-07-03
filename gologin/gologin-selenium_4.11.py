dcc9bec403a593944b1675cc1812254ef2ca0b60
AlexName1 <at146@ya.ru>
2024-07-03 16:58:08 +0300

    change: ruff formated

--- gologin/gologin-selenium_4.11.py
+++ gologin/gologin-selenium_4.11.py
@@ -5,17 +5,14 @@ from gologin import GoLogin
 from gologin import getRandomPort
 from selenium.webdriver.chrome.service import Service
 
-gl = GoLogin({
-	"token": "yU0token",
-	"profile_id": "yU0Pr0f1leiD"
-	})
+gl = GoLogin({"token": "yU0token", "profile_id": "yU0Pr0f1leiD"})
 
 if platform == "linux" or platform == "linux2":
-	chrome_driver_path = "./chromedriver"
+    chrome_driver_path = "./chromedriver"
 elif platform == "darwin":
-	chrome_driver_path = "./mac/chromedriver"
+    chrome_driver_path = "./mac/chromedriver"
 elif platform == "win32":
-	chrome_driver_path = "chromedriver.exe"
+    chrome_driver_path = "chromedriver.exe"
 
 debugger_address = gl.start()
 service = Service(executable_path=chrome_driver_path)
