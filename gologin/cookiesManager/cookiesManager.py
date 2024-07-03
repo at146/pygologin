8316d0ea8d8b3fc02988085d62726da7d6013918
AlexName1 <at146@ya.ru>
2024-07-03 16:58:08 +0300

    change: ruff formated

--- gologin/cookiesManager/cookiesManager.py
+++ gologin/cookiesManager/cookiesManager.py
@@ -8,10 +8,10 @@ from os import access, F_OK
 MAX_SQLITE_VARIABLES = 1
 
 SAME_SITE = {
-    -1: 'unspecified',
-    0: 'no_restriction',
-    1: 'lax',
-    2: 'strict',
+    -1: "unspecified",
+    0: "no_restriction",
+    1: "lax",
+    2: "strict",
 }
 
 COOKIE_ROW_COLUMN_NAMES = [
@@ -35,64 +35,92 @@ COOKIE_ROW_COLUMN_NAMES = [
     'last_update_utc',
 ]
 
-class CookiesManager():
+class CookiesManager:
     def __init__(self, *args, **kwargs):
-        self.profile_id = kwargs.get('profile_id')
-        self.tmpdir = kwargs.get('tmpdir')
+        self.profile_id = kwargs.get("profile_id")
+        self.tmpdir = kwargs.get("tmpdir")
 
     def get_db(self):
         connection_opts = {"database": self.get_cookies_file_path()}
         # print('FILEPATH', self.get_cookies_file_path())
         return sqlite3.connect(**connection_opts)
 
-    def get_chunked_insert_values(self, cookies_arr: List[dict]) -> List[Tuple[str, List]]:
+    def get_chunked_insert_values(
+        self, cookies_arr: List[dict]
+    ) -> List[Tuple[str, List]]:
         today_unix = int(datetime.datetime.now().timestamp())
-        chunked_cookies_arr = [cookies_arr[i:i + MAX_SQLITE_VARIABLES] for i in range(0, len(cookies_arr), MAX_SQLITE_VARIABLES)]
+        chunked_cookies_arr = [
+            cookies_arr[i : i + MAX_SQLITE_VARIABLES]
+            for i in range(0, len(cookies_arr), MAX_SQLITE_VARIABLES)
+        ]
 
         # for cookies in chunked_cookies_arr:
-            # print('COOKIES::::::', cookies)
+        # print('COOKIES::::::', cookies)
         result = []
 
         for cookies in chunked_cookies_arr:
-            query_placeholders = ", ".join(["(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"] * len(cookies))
+            query_placeholders = ", ".join(
+                ["(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"]
+                * len(cookies)
+            )
             query = f"insert or replace into cookies (creation_utc, host_key, top_frame_site_key, name, value, encrypted_value, path, expires_utc, is_secure, is_httponly, last_access_utc, has_expires, is_persistent, priority, samesite, source_scheme, source_port, last_update_utc) values {query_placeholders}"
 
             query_params = []
             for cookie in cookies:
                 creation_date = cookie.get("creationDate", today_unix)
-                expiration_date = 0 if cookie.get("session", False) else cookie.get("expirationDate", 0)
+                expiration_date = (
+                    0
+                    if cookie.get("session", False)
+                    else cookie.get("expirationDate", 0)
+                )
                 encrypted_value = cookie["value"]
                 value = cookie["value"]
-                samesite = next(key for key, value in SAME_SITE.items() if value == cookie.get("sameSite", "-1"))
-                is_secure = 1 if cookie["name"].startswith("__Host-") or cookie["name"].startswith("__Secure-") else int(cookie.get("secure", 0))
+                samesite = next(
+                    key
+                    for key, value in SAME_SITE.items()
+                    if value == cookie.get("sameSite", "-1")
+                )
+                is_secure = (
+                    1
+                    if cookie["name"].startswith("__Host-")
+                    or cookie["name"].startswith("__Secure-")
+                    else int(cookie.get("secure", 0))
+                )
                 source_scheme = 2 if is_secure == 1 else 1
                 source_port = 443 if is_secure == 1 else 80
-                is_persistent = 0 if cookie.get("session") else 1 if expiration_date != 0 else 0
+                is_persistent = (
+                    0 if cookie.get("session") else 1 if expiration_date != 0 else 0
+                )
 
-                if cookie.get("domain") == ".mail.google.com" and cookie["name"] == "COMPASS":
+                if (
+                    cookie.get("domain") == ".mail.google.com"
+                    and cookie["name"] == "COMPASS"
+                ):
                     expiration_date = 0
                     is_persistent = 0
 
-                query_params.append((
-                    creation_date,
-                    cookie.get("domain", ""),
-                    "",
-                    cookie["name"],
-                    "",
-                    cookie["value"],
-                    cookie.get("path", ""),
-                    expiration_date,
-                    is_secure,
-                    int(cookie.get("httpOnly", 0)),
-                    0,
-                    0 if expiration_date == 0 else 1,
-                    is_persistent,
-                    1,
-                    samesite,
-                    source_scheme,
-                    source_port,
-                    0
-                ))
+                query_params.append(
+                    (
+                        creation_date,
+                        cookie.get("domain", ""),
+                        "",
+                        cookie["name"],
+                        "",
+                        cookie["value"],
+                        cookie.get("path", ""),
+                        expiration_date,
+                        is_secure,
+                        int(cookie.get("httpOnly", 0)),
+                        0,
+                        0 if expiration_date == 0 else 1,
+                        is_persistent,
+                        1,
+                        samesite,
+                        source_scheme,
+                        source_port,
+                        0,
+                    )
+                )
 
             result.append((query, query_params))
 
@@ -104,7 +132,7 @@ class CookiesManager():
 
         try:
             db = self.get_db()
-            cookies_rows = db.execute('select * from cookies')
+            cookies_rows = db.execute("select * from cookies")
             cookies_rows = cookies_rows.fetchall()
             for row in cookies_rows:
                 row_data = dict(zip(COOKIE_ROW_COLUMN_NAMES, row))
@@ -123,7 +151,7 @@ class CookiesManager():
                     'creationDate': self.ldap_to_unix(row_data['creation_utc']),
                 })
         except Exception as error:
-            print('load_cookies_from_file', error)
+            print("load_cookies_from_file", error)
             raise error
         finally:
             if db:
@@ -136,7 +164,9 @@ class CookiesManager():
             return unixtime
 
         win32filetime = datetime.utcfromtimestamp(0)
-        win32filetime_utc = (win32filetime - datetime.datetime(1601, 1, 1)).total_seconds()
+        win32filetime_utc = (
+            win32filetime - datetime.datetime(1601, 1, 1)
+        ).total_seconds()
 
         sum_ = unixtime - win32filetime_utc
 
@@ -151,7 +181,9 @@ class CookiesManager():
 
         _ldap = ldap
         if ldap_length < 18:
-            _ldap = int(ldap_str + '0' * (18 - ldap_length))  # Padding zeros to the integer part
+            _ldap = int(
+                ldap_str + "0" * (18 - ldap_length)
+            )  # Padding zeros to the integer part
 
         # Create a datetime object for January 1, 1601
         win32_epoch = datetime.datetime(1601, 1, 1, tzinfo=datetime.timezone.utc)
@@ -161,8 +193,8 @@ class CookiesManager():
         return (_ldap / 10000 + win32filetime_epoch) / 1000
 
     def build_cookie_url(self, domain: str, secure: bool, path: str) -> str:
-        domain_without_dot = domain[1:] if domain.startswith('.') else domain
-        protocol = 'https://' if secure else 'http://'
+        domain_without_dot = domain[1:] if domain.startswith(".") else domain
+        protocol = "https://" if secure else "http://"
 
         return protocol + domain_without_dot + path
 
@@ -170,11 +202,15 @@ class CookiesManager():
         if chunk_size <= 0:
             return []
 
-        return [arr[i:i + chunk_size] for i in range(0, len(arr), chunk_size)]
+        return [arr[i : i + chunk_size] for i in range(0, len(arr), chunk_size)]
 
     def get_cookies_file_path(self) -> str:
-        base_cookies_file_path = os.path.join(self.tmpdir, f'gologin_{self.profile_id}', 'Default', 'Cookies')
-        bypass_cookies_file_path = os.path.join(self.tmpdir, f'gologin_{self.profile_id}', 'Default', 'Network', 'Cookies')
+        base_cookies_file_path = os.path.join(
+            self.tmpdir, f"gologin_{self.profile_id}", "Default", "Cookies"
+        )
+        bypass_cookies_file_path = os.path.join(
+            self.tmpdir, f"gologin_{self.profile_id}", "Default", "Network", "Cookies"
+        )
 
         if access(base_cookies_file_path, F_OK):
             return base_cookies_file_path
@@ -184,11 +220,11 @@ class CookiesManager():
 
         return base_cookies_file_path
 
-
     def write_cookies_to_file(self, cookies):
-        print ('write_cookies_to_file')
+        print("write_cookies_to_file")
         result_cookies = [
-            {'value': base64.b64encode(cookie['value'].encode()), **cookie} for cookie in cookies #plain
+            {"value": base64.b64encode(cookie["value"].encode()), **cookie}
+            for cookie in cookies  # plain
             # {
             #     **cookie,
             #     "value": base64.b64encode(bytearray(cookie["value"]["data"])).decode('utf-8')
@@ -206,7 +242,7 @@ class CookiesManager():
                         res = cursor.execute(query, params)
 
             else:
-                query = 'delete from cookies'
+                query = "delete from cookies"
                 cursor.execute(query)
 
             db.commit()
@@ -214,7 +250,9 @@ class CookiesManager():
             self.load_cookies_from_file()
             db.close()
         except Exception as error:
-            print('write_cookies_to_file exception:', error, error.__traceback__.tb_lineno)
+            print(
+                "write_cookies_to_file exception:", error, error.__traceback__.tb_lineno
+            )
             raise error
         finally:
             if db:
--- gologin/gologin-local.py
+++ gologin/gologin-local.py
@@ -5,19 +5,21 @@ from selenium.webdriver.chrome.options import Options
 from gologin import GoLogin
 
 
-gl = GoLogin({
-	"token": "yU0token",
-	"profile_id": "yU0Pr0f1leiD",
-	"local": True,
-	"credentials_enable_service": False,
-	})
+gl = GoLogin(
+    {
+        "token": "yU0token",
+        "profile_id": "yU0Pr0f1leiD",
+        "local": True,
+        "credentials_enable_service": False,
+    }
+)
 
 if platform == "linux" or platform == "linux2":
-	chrome_driver_path = "./chromedriver"
+    chrome_driver_path = "./chromedriver"
 elif platform == "darwin":
-	chrome_driver_path = "./mac/chromedriver"
+    chrome_driver_path = "./mac/chromedriver"
 elif platform == "win32":
-	chrome_driver_path = "chromedriver.exe"
+    chrome_driver_path = "chromedriver.exe"
 
 debugger_address = gl.start()
 chrome_options = Options()
