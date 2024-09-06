3402dbab438283b5ed15c158f0f4b0a9e8098d90
AlexName1 <at146@ya.ru>
2024-09-06 23:11:57 +0300

    add: test change: python 3.8

--- pygologin/__init__.py
+++ pygologin/__init__.py
@@ -2,4 +2,8 @@ from .gologin import GoLogin
 from .gologin import getRandomPort
 from .__meta__ import __version__
 
-__all__ = ("GoLogin", "getRandomPort", "__version__",)
+__all__ = (
+    "GoLogin",
+    "getRandomPort",
+    "__version__",
+)
